d53affaa8fe062515a8f368e9828ce7bc6aae8f1
n4505-ndensan <n4505@ndensan.co.jp>
2025-10-03 12:14:44 +0900

    get rid of unnecessary script

--- web/vercel-build.sh
+++ web/vercel-build.sh
@@ -2,12 +2,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
-# Build MNIST WASM module
-cd ../model
-wasm-pack build --target bundler --out-dir ../web/src/lib/mnist -- --features mnist
-# Build CIFAR-10 WASM module
-wasm-pack build --target bundler --out-dir ../web/src/lib/cifar10 -- --features cifar10
-
 # Return to web directory and build the app
 cd ../web
 npm run build
